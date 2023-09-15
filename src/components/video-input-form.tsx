aa77e5e1dc450e0996139c79118547076c89b382
Henrique Yule <yzhenrique@gmail.com>
2023-09-14 21:28:25 -0400

    integração com backend e transcrição de vídeo

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -6,6 +6,7 @@ import { Button } from "./ui/button";
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
 import { getFFmpeg } from "@/lib/ffmpeg";
 import { fetchFile } from '@ffmpeg/util';
+import { api } from "@/lib/axios";
 
 export function VideoInputForm(){
 
@@ -79,7 +80,19 @@ export function VideoInputForm(){
 
         const audioFile = await convertVideoToAudio(videoFile);
 
-        console.log(audioFile, prompt);
+        const data = new FormData();
+
+        data.append('file', audioFile)
+
+        const response = await api.post('/videos', data)
+
+        const videoId = response.data.video.id;
+
+        await api.post(`/videos/${videoId}/transcription`, {
+            prompt,
+        });
+
+        console.log('finalizado');
     }
 
     const previewURL = useMemo(() => {
